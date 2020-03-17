2e5a806730eebac9ab29b3ffa009a95776c4e41c
danialvesb <dan78615>
2020-03-16 22:27:37 -0300

    Pesquisa referencia adicionada.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,15 +3,22 @@ import {View, StyleSheet, Button, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Filters from '../Filters';
 import { CustomDrawerContent } from '../../Menu';
+import Search from './Search';
+
 
 export  default props =>
     <View style={styles.container}>
         <View style={styles.menu}>
-            <TouchableOpacity onPress={() => props.navigate.openDrawer()}>
-                <View style={{margin: 10, }}>
-                    <Icon name="bars" size={30} color="#FFF" />
-                </View>
-            </TouchableOpacity>
+            <View style={{flex: 1, flexDirection: 'row'}}>
+                <TouchableOpacity onPress={() => props.navigate.openDrawer()} >
+                    <View style={{margin: 10, }}>
+                        <Icon name="bars" size={30} color="#FFF" />
+                    </View>
+                </TouchableOpacity>
+            </View>
+            <View style={{flex: 5 }}>
+                <Search></Search>
+            </View>
 
         </View>
         <View style={styles.searchs}>
@@ -31,8 +38,9 @@ const styles = StyleSheet.create({
     },
     menu: {
         flex: 1,
+        flexDirection: 'row',
         justifyContent: 'center',
-        backgroundColor: '#2e1216'
+        backgroundColor: 'rgb(36, 41, 46)'
     },
     home: {
 
