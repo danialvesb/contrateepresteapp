64f0af504339a919862df77700f7788b4473371a
danialvesb <dan78615>
2020-03-21 14:26:40 -0300

    Tela de solicitar terminada

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Home from './screens/Home'
 import Auth from './screens/Auth'
 import Menu from './Menu'
+import RequestWork from './screens/RequestWork'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 // createDrawerNavigator()
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,9 +13,10 @@ const Stack = createStackNavigator();
 export  default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Menu" headerMode='none'>
-                <Stack.Screen name="Menu" component={ Menu }  options={{ title: 'My home' }} />
+            <Stack.Navigator initialRouteName="RequestWorkPage" headerMode='none'>
+                <Stack.Screen name="Menu" component={ Menu } />
                 <Stack.Screen name="AuthPage" component={ Auth } />
+                <Stack.Screen name="RequestWorkPage" component={ RequestWork } />
             </Stack.Navigator>
             {/*<Drawer.Navigator  drawerContent={props => <CustomDrawerContent {...props} />}>*/}
             {/*    <Drawer.Screen name="InÍcio" component={Home}/>*/}
