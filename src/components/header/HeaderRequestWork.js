b4f3b22915dfbed524ffe1fed6ddedc26fc87be7
danialvesb <dan78615>
2020-03-21 14:41:57 -0300

    Efetuada mudanças de acoplamento

--- src/components/header/HeaderRequestWork.js
+++ src/components/header/HeaderRequestWork.js
@@ -9,21 +9,7 @@ export  default props => {
                     <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)', flex: 5}}>Informações</Text>
                     <TouchableOpacity style={{backgroundColor: 'rgba(36,41,46,0.35)', flex: 1, margin: 1, alignItems: 'center', justifyContent: 'center'}}><Text>Voltar</Text></TouchableOpacity>
             </View>
-            <View style={styles.content}>
-                <View style={styles.data}>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
-                </View>
-                <View style={styles.options}>
-                    <TouchableOpacity style={{backgroundColor: 'rgba(36,41,46,0.35)', margin: 1, alignItems: 'center', justifyContent: 'center'}}><Text style={{fontSize: 20}}>Solicitar</Text></TouchableOpacity>
-                </View>
 
-                <View style={styles.description}>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)', flex: 5}}>Descrição</Text>
-                </View>
-            </View>
         </View>
     )
 }
@@ -45,44 +31,4 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(0,0,0,0.33)'
 
     },
-    content: {
-        borderWidth: 0.5,
-        borderColor: 'rgb(36, 41, 46)',
-        width: '95%',
-        height: '80%',
-        margin: 5,
-        borderRadius: 4,
-        flexDirection: 'row',
-        flexWrap: 'wrap'
-
-    },
-    data: {
-        borderWidth: 0.5,
-        borderColor: 'rgb(36, 41, 46)',
-        width: '70%',
-        height: '70%',
-        margin: 5,
-        borderRadius: 4,
-    },
-    options: {
-        flex: 2,
-        borderWidth: 0.5,
-        borderColor: 'rgb(36, 41, 46)',
-        width: '70%',
-        height: '20%',
-        margin: 5,
-        borderRadius: 4,
-
-    },
-    description: {
-
-        borderWidth: 0.5,
-        borderColor: 'rgb(36, 41, 46)',
-        width: '70%',
-        height: '20%',
-        margin: 5,
-        borderRadius: 4,
-
-
-    }
 })
--- src/screens/RequestWork.js
+++ src/screens/RequestWork.js
@@ -5,13 +5,82 @@ import HeaderRequestWork from '../components/header/HeaderRequestWork';
 export  default props => {
     return (
         <View style={ styles.container }>
-            <HeaderRequestWork></HeaderRequestWork>
-        </View>
+            <View style={styles.header}>
+                <HeaderRequestWork></HeaderRequestWork>
+            </View>
+            <View style={styles.content}>
+                <View style={styles.data}>
+                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
+                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
+                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
+                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Descrição</Text>
+                </View>
+                <View style={styles.options}>
+                    <TouchableOpacity style={{backgroundColor: 'rgba(36,41,46,0.35)', margin: 1, alignItems: 'center', justifyContent: 'center'}}><Text style={{fontSize: 20}}>Solicitar</Text></TouchableOpacity>
+                </View>
+
+                <View style={styles.description}>
+                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)', flex: 5}}>Descrição</Text>
+                </View>
+            </View>
+            </View>
+
+
     )
 }
 
 const styles = StyleSheet.create({
+    header: {
+        margin: 2,
+        padding: 5,
+        width: '100%',
+        flexDirection: 'row',
+        backgroundColor: 'rgba(0,0,0,0.33)'
+    },
     container: {
-        flex: 1
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+
+    },
+    content: {
+        borderWidth: 0.5,
+        borderColor: 'rgb(36, 41, 46)',
+        width: '95%',
+        height: '80%',
+        margin: 5,
+        borderRadius: 4,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+
+    },
+    data: {
+        borderWidth: 0.5,
+        borderColor: 'rgb(36, 41, 46)',
+        width: '70%',
+        height: '70%',
+        margin: 5,
+        borderRadius: 4,
+    },
+    options: {
+        flex: 2,
+        borderWidth: 0.5,
+        borderColor: 'rgb(36, 41, 46)',
+        width: '70%',
+        height: '20%',
+        margin: 5,
+        borderRadius: 4,
+
+    },
+    description: {
+
+        borderWidth: 0.5,
+        borderColor: 'rgb(36, 41, 46)',
+        width: '70%',
+        height: '20%',
+        margin: 5,
+        borderRadius: 4,
+
+
     }
 })
