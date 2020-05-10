b805ff2145fcd4600964d7fbc75b92b5a16ed658
danialvesb <dan78615>
2020-05-10 15:44:42 -0300

    Tela de andamento das solicitações finalizada

--- src/screens/client/Solicitation.js
+++ src/screens/client/Solicitation.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import {View, Text, StyleSheet, TouchableOpacity, ScrollView} from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import {Caption} from 'react-native-paper'
+import {Avatar, Caption} from 'react-native-paper';
 
 export default props => {
     return (
         <View style={styles.containerStyle}>
             <View style={styles.bodyStyle}>
                 <View style={styles.left}>
+                    <Text style={styles.fieldStyle}>{props.data.type_service}</Text>
+                    <Text style={styles.fieldStyle}>{props.data.amount}</Text>
+                </View>
+                <View style={styles.rigth}>
                     {props.data.status === "pending" ?
                         <View>
                             <Icon name="clock-o" color='#FFC925' size={40}></Icon>
-                            <Caption style={styles.caption}>Aguardando resposta</Caption>
+                            <Caption>Pendente</Caption>
                         </View>
                         :
                         <TouchableOpacity>
-                            <Icon name="send-o" color='#50B767' size={40}/>
-                            <Caption style={styles.caption}>Enviar mensagem</Caption>
+                            <Avatar.Image source={require('../../../assets/icons/message-icon.png')} size={40}/>
+                            <Caption>Chat</Caption>
                         </TouchableOpacity>
                     }
                 </View>
-                <View style={styles.rigth}>
-                    <Text style={styles.fieldStyleBold}>{props.data.type_service}</Text>
-                    <Text style={styles.fieldStyleBold}>{props.data.amount}</Text>
-                </View>
+
             </View>
             <View style={styles.footer}>
                 <Text style={styles.fieldStyle}>{props.data.solicitation_message}</Text>
@@ -39,18 +40,47 @@ export default props => {
                     </TouchableOpacity>
                 </ScrollView>
             </View>
-            <View style={styles.timeLineContainer}>
-                <View style={styles.timeLine}>
-                    <Text style={styles.fieldStyle}>Time line-------------------------------------></Text>
+            {props.data.status === "pending" &&
+                <View style={styles.timeLineContainer}>
+                    <View style={styles.timeLineStepOne}>
+                        <Text style={styles.textTimeLine}>Pendente</Text>
+                    </View>
                 </View>
-            </View>
-
+            }
+            {props.data.status === "accepted" &&
+                <View style={styles.timeLineContainer}>
+                    <View style={styles.timeLineStepOne}>
+                        <Text style={styles.textTimeLine}>Pendente</Text>
+                    </View>
+                    <View style={styles.timeLineStepTwo}>
+                        <Text style={styles.textTimeLine}>Trabalhando</Text>
+                    </View>
+                </View>
+            }
+            {props.data.status === "finished" &&
+                <View style={styles.timeLineContainer}>
+                    <View style={styles.timeLineStepOne}>
+                        <Text style={styles.textTimeLine}>Pendente</Text>
+                    </View>
+                    <View style={styles.timeLineStepTwo}>
+                        <Text style={styles.textTimeLine}>Trabalhando</Text>
+                    </View>
+                    <View style={styles.timeLineStepThree}>
+                        <Text style={styles.textTimeLine}>Finalizado</Text>
+                    </View>
+                </View>
+            }
             <View style={styles.buttons}>
                 {props.data.status === "pending" &&
                     <TouchableOpacity style={styles.buttonStyle}>
                         <Text style={styles.buttonText}>Cancelar</Text>
                     </TouchableOpacity>
 
+                }
+                {props.data.status === "finished" &&
+                    <TouchableOpacity style={styles.buttonStyleFinished}>
+                        <Text style={styles.buttonText}>Avaliar serviço</Text>
+                    </TouchableOpacity>
                 }
             </View>
         </View>
@@ -73,26 +103,28 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 5,
         borderColor: 'rgba(26,21,21,0.23)',
-        height: 350
+        height: 380
     },
     bodyStyle: {
-        flex: 1,
+        flex: 2,
+        width: "100%",
         flexDirection: 'row',
+        justifyContent: 'space-between',
         flexWrap: 'wrap'
     },
     left: {
-        flex: 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 2,
-        marginTop: 1,
+        padding: 5,
+        margin: 5,
+        borderWidth: 1,
+        borderColor: "rgba(0,0,0,0.13)",
+        borderRadius: 10
     },
     rigth: {
-        flex: 3,
-        padding: 2,
+        padding: 5,
+        margin: 5,
     },
     footer: {
-        flex: 1,
+        flex: 2,
         marginTop: 9,
         padding: 1,
         borderWidth: 1,
@@ -110,7 +142,17 @@ const styles = StyleSheet.create({
         padding: 3
     },
     buttonStyle: {
-        backgroundColor: 'red',
+        backgroundColor: '#E75B65',
+        padding: 4,
+        margin: 5,
+        borderRadius: 10,
+        width: '45%',
+        height: "70%",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    buttonStyleFinished: {
+        backgroundColor: '#187FF5',
         padding: 4,
         margin: 5,
         borderRadius: 10,
@@ -122,13 +164,38 @@ const styles = StyleSheet.create({
     timeLineContainer: {
         flex: 1,
         flexDirection: 'row',
-        width: '100%',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '97%',
         margin: 4,
         borderBottomWidth: 1,
         borderColor: 'rgba(26,21,21,0.23)',
     },
-    timeLine: {
-        flex: 1
+    timeLineStepOne: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "center",
+        backgroundColor: "#FF9431",
+        width: "33%",
+        height: 25
+    },
+    timeLineStepTwo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "center",
+        backgroundColor: "#34B2E4",
+        width: "33%",
+        height: 25
+    },
+    timeLineStepThree: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "center",
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        backgroundColor: "#69DAAC",
+        width: "33%",
+        height: 25
     },
     buttons: {
         flex: 1
@@ -154,5 +221,10 @@ const styles = StyleSheet.create({
     caption: {
         fontFamily: "Montserrat-Regular",
         fontWeight: "400",
+    },
+    textTimeLine: {
+        color: "#FFF",
+        fontFamily: "Montserrat-Italic",
+        textAlign: "center",
     }
 })
