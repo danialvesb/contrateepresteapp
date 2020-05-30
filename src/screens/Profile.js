d5d92f55b4b33377055d6c964cf37e447b7e6dfc
danialvesb <dan78615>
2020-05-30 14:27:30 -0300

    Ajustes

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import {View, StyleSheet, ScrollView, Text, TouchableOpacity} from 'react-native'
-import {Avatar, Caption} from 'react-native-paper'
+import {View, StyleSheet, ScrollView,TouchableOpacity} from 'react-native'
+import {Avatar} from 'react-native-paper'
 import axios from 'axios'
 import {server, showError} from '../common'
 import AsyncStorage from '@react-native-community/async-storage'
 import { UserConsumer } from '../Navigator'
 import commonStyles from '../commonStyles';
+import { Divider, Text } from 'react-native-paper';
+import { List } from 'react-native-paper';
+import {Input} from 'react-native-elements';
 
 const initialState = {
     name: 'Daniel Alves',
@@ -18,6 +21,7 @@ const initialState = {
     typeAccount: 'Prestador',
     isLogged: false,
     user: {},
+    stageUpdate: false,
     auth: {
         isLogged: false,
         user: {}
@@ -57,34 +61,137 @@ export default class Profile extends Component{
         return (
             <UserConsumer>
                 {value => {
-                    console.log(value.auth.user)
                     const {auth} = value
                     return (
                         <View style={styles.containerStyle}>
                             <ScrollView>
                                 <View style={styles.headerStyle}>
-                                    <Avatar.Image source={{uri: `http://192.168.3.103:8000/api/me/_image/profile/${auth.user.photo}`,}} size={80}/>
+                                    {!this.state.stageUpdate ?
+                                        <Avatar.Image
+                                            source={{uri: `http://192.168.3.103:8000/api/me/_image/profile/${auth.user.photo}`,}}
+                                            size={80}/>
+                                            :
+                                        <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center', margin: 3}}>
+                                            <Avatar.Image
+                                                source={{uri: `http://192.168.3.103:8000/api/me/_image/profile/${auth.user.photo}`,}}
+                                                size={60}/>
+                                                <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'gray', borderRadius: 5, padding: 2}}>
+                                                    <Text style={ commonStyles.textButtonsStyle }>
+                                                        Carregar imagem
+                                                    </Text>
+                                                </View>
+                                        </TouchableOpacity>
+                                    }
                                 </View>
                                 <View style={styles.contentStyle}>
                                     <View style={styles.containerStyleText}>
                                         <Text style={styles.labelStyleText}>{ auth.user.name }</Text>
                                     </View>
+                                    <Divider />
                                     <View style={styles.containerStyleCaption}>
-                                        <Caption style={styles.labelStyleCaption}>{ auth.user.email }</Caption>
-                                        <Caption style={styles.labelStyleCaption}>{ auth.user.mobile }</Caption>
-                                        <Caption style={styles.labelStyleCaption}>{ auth.user.city }</Caption>
-                                        <Caption style={styles.labelStyleCaption}>{ auth.user.uf }</Caption>
-                                        <Caption style={styles.labelStyleCaption}>{ auth.user.district }</Caption>
-                                        <Caption style={styles.labelStyleCaption}>{ auth.user.group }</Caption>
+                                        {!this.state.stageUpdate ?
+                                            <List.Item
+                                                title="Email:"
+                                                description={auth.user.email}
+                                            />
+                                            :
+                                            <List.Item
+                                                left={props => <View {...props} style={{width: '100%'}}>
+                                                    <Text>Email:</Text>
+                                                    <Input inputStyle={{paddingBottom: 0, marginBottom: 0}} value={auth.user.email} disabled={true}/>
+                                                </View>}
+                                            />
+                                        }
+                                        <Divider />
+                                        {!this.state.stageUpdate ?
+                                            <List.Item
+                                                title="Celular:"
+                                                description={auth.user.mobile}
+                                            />
+                                            :
+                                            <List.Item
+                                                left={props => <View {...props} style={{width: '100%'}}>
+                                                    <Text>Celular:</Text>
+                                                    <Input value={auth.user.mobile} inputStyle={{paddingBottom: 0, marginBottom: 0}}/>
+                                                </View>}
+                                            />
+                                        }
+                                        <Divider />
+                                        {!this.state.stageUpdate ?
+                                            <List.Item
+                                                title="Cidade:"
+                                                description={auth.user.city}
+                                            />
+                                            :
+                                            <List.Item
+                                                left={props => <View {...props} style={{width: '100%'}}>
+                                                    <Text>Cidade:</Text>
+                                                    <Input value={auth.user.city} inputStyle={{paddingBottom: 0, marginBottom: 0}}/>
+                                                </View>}
+                                            />
+                                        }
+                                        <Divider />
+                                        {!this.state.stageUpdate ?
+                                            <List.Item
+                                                title="Estado:"
+                                                description={auth.user.uf}
+                                            />
+                                            :
+                                            <List.Item
+                                                left={props => <View {...props} style={{width: '100%'}}>
+                                                    <Text>Estado:</Text>
+                                                    <Input value={auth.user.uf} inputStyle={{paddingBottom: 0, marginBottom: 0}}/>
+                                                </View>}
+                                            />
+                                        }
+                                        <Divider />
+                                        {!this.state.stageUpdate ?
+                                            <List.Item
+                                                title="Bairro:"
+                                                description={auth.user.district}
+                                            />
+                                            :
+                                            <List.Item
+                                                left={props => <View {...props} style={{width: '100%'}}>
+                                                    <Text>Bairro:</Text>
+                                                    <Input value={auth.user.district} inputStyle={{paddingBottom: 0, marginBottom: 0}}/>
+                                                </View>}
+                                            />
+                                        }
+
+                                        <Divider />
+                                        {!this.state.stageUpdate ?
+                                            <List.Item
+                                                title="Tipo de perfil:"
+                                                description={auth.user.group}
+                                            />
+                                            :
+                                            <List.Item
+                                                left={props => <View {...props} style={{width: '100%'}}>
+                                                    <Text>Tipo de pefil:</Text>
+                                                    <Input inputStyle={{paddingBottom: 0, marginBottom: 0}}/>
+                                                </View>}
+                                            />
+                                        }
                                     </View>
                                 </View>
+                                <Divider />
                                 <View style={styles.footer}>
-                                    <TouchableOpacity style={styles.buttonStyleAcept}>
-                                        <Text style={ commonStyles.textButtonsStyle }>Editar informações</Text>
+                                    {!this.state.stageUpdate &&
+                                        <TouchableOpacity style={styles.buttonStyleAcept} onPress={ () => { this.setState({stageUpdate: true}) }}>
+                                            <Text style={commonStyles.textButtonsStyle}>Editar informações</Text>
+                                        </TouchableOpacity>
+                                    }
+                                    {!this.state.stageUpdate &&
+                                        <TouchableOpacity style={styles.buttonStyleRecuse} onPress={() => { this.logout(value) }}>
+                                            <Text style={ commonStyles.textButtonsStyle }>Sair</Text>
+                                        </TouchableOpacity>
+                                    }
+                                    {this.state.stageUpdate &&
+                                    <TouchableOpacity style={styles.buttonStyleSave} onPress={() => { this.setState({stageUpdate: false}) }}>
+                                        <Text style={ commonStyles.textButtonsStyle }>Salvar</Text>
                                     </TouchableOpacity>
-                                    <TouchableOpacity style={styles.buttonStyleRecuse} onPress={() => { this.logout(value) }}>
-                                        <Text style={ commonStyles.textButtonsStyle }>Sair</Text>
-                                    </TouchableOpacity>
+                                    }
                                 </View>
                             </ScrollView>
                         </View>
@@ -131,20 +238,16 @@ const styles = StyleSheet.create({
         padding: 3
     },
     labelStyleText: {
-        borderBottomWidth: 1,
-        borderColor: 'rgba(36,41,46,0.23)',
         fontSize: 24,
         width: '50%',
     },
     labelStyleCaption: {
-        borderBottomWidth: 1,
-        borderColor: 'rgba(36,41,46,0.23)',
         fontSize: 20,
         marginTop: 20
     },
     footer: {
         flex: 1,
-        margin: 0,
+        marginTop: 3,
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'center',
@@ -162,4 +265,11 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 1,
     },
+    buttonStyleSave: {
+        width: 300,
+        backgroundColor: 'rgba(28,116,72,0.76)',
+        padding: 10,
+        margin: 5,
+        borderRadius: 10
+    }
 })
