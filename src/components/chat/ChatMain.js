c73428764d7d1ed53fb01b2b5e42da39f44f49a4
danialvesb <dan78615>
2020-06-14 21:40:11 -0300

    Ajustes no chat

--- src/components/chat/ChatMain.js
+++ src/components/chat/ChatMain.js
@@ -5,9 +5,8 @@ import emojiUtils from 'emoji-utils'
 import SlackMessage from './SlackMessage'
 import axios from 'axios'
 import Pusher from 'pusher-js/react-native'
-import {server, showError, showMessage} from '../../common';
-import {UserConsumer} from '../../Navigator';
-import AsyncStorage from '@react-native-community/async-storage';
+import {server, showError, showMessage} from '../../common'
+import AsyncStorage from '@react-native-community/async-storage'
 
 // Enable pusher logging - don't include this in production
 Pusher.logToConsole = true
@@ -22,24 +21,24 @@ const pusherConfig = {
 }
 
 export default class ChatMain extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.pusher = new Pusher(pusherConfig.key, pusherConfig); // (1)
-    //     this.chatChannel = this.pusher.subscribe('chat'); // (2)
-    //     this.chatChannel.bind('pusher:subscription_succeeded', () => { // (3)
-    //         this.chatChannel.bind('join', (data) => { // (4)
-    //             this.handleJoin(data.name)
-    //         })
-    //         this.chatChannel.bind('part', (data) => { // (5)
-    //             this.handlePart(data.name)
-    //         })
-    //         this.chatChannel.bind('message', (data) => { // (6)
-    //             this.handleMessage(data.name, data.message)
-    //         })
-    //     })
-    //
-    //     this.handleSendMessage = this.onSendMessage.bind(this) // (9)
-    // }
+    constructor(props) {
+        super(props)
+        this.pusher = new Pusher(pusherConfig.key, pusherConfig) // (1)
+        this.chatChannel = this.pusher.subscribe('chat') // (2)
+        this.chatChannel.bind('pusher:subscription_succeeded', () => { // (3)
+            this.chatChannel.bind('join', (data) => { // (4)
+                this.handleJoin(data.name)
+            })
+            this.chatChannel.bind('part', (data) => { // (5)
+                this.handlePart(data.name)
+            })
+            this.chatChannel.bind('App\\Events\\MessageSent', (data) => { // (6)
+                this.handleMessage(data)
+            })
+        })
+
+        this.handleSendMessage = this.onSendMessage.bind(this) // (9)
+    }
     state = {
         messages: [],
     }
@@ -60,12 +59,27 @@ export default class ChatMain extends React.Component {
         })
     }
 
-    handleMessage(name, message) { // (6)
-        const messages = this.state.messages.slice()
-        messages.push({action: 'message', name: name, message: message})
-        this.setState({
-            messages: messages
-        })
+    handleMessage(data) { // (6)
+        console.log(JSON.stringify(data))
+
+        // const messages = this.state.messages.slice()
+        // messages.push({action: 'message', name: name, message: message})
+        // this.setState({
+        //     messages: messages
+        // })
+        // if ()
+        // const messages = response.data.map((item, index) => (
+        //     {
+        //         _id: item.id,
+        //         text: item.text,
+        //         createdAt: item.created_at,
+        //         user: {
+        //             _id: item.from_user,
+        //             name: item.from_user_name,
+        //             avatar: `http://192.168.3.103:8000/api/me/_image/profile/${item.from_user_avatar}`
+        //         }
+        //     }
+        // ))
     }
 
     //
@@ -76,24 +90,8 @@ export default class ChatMain extends React.Component {
     // }
     //
     componentDidMount() {
-
-        console.log(JSON.stringify(this.props.route.params))
-
         this.loadMensages(this.props.route.params.item.id)
-        // this.setState({
-        //     messages: [
-        //         {
-        //             _id: 1,
-        //             text: 'Hello developer!!!',
-        //             createdAt: new Date(),
-        //             user: {
-        //                 _id: 2,
-        //                 name: 'React Native',
-        //                 avatar: 'https://placeimg.com/140/140/any',
-        //             },
-        //         },
-        //     ],
-        // })
+
     }
 
     async loadMensages(solicitationId) {
@@ -115,9 +113,10 @@ export default class ChatMain extends React.Component {
                     }
                 }
             ))
+
             this.setState({messages})
         }).catch(err => {
-            showError(JSON.stringify(err))
+            showError(JSON.stringify(JSON.stringify(err)))
         })
     }
 
@@ -140,7 +139,7 @@ export default class ChatMain extends React.Component {
         console.log(this.state.messages)
         // console.log(JSON.stringify(messages))
     }
-    [{"text":"Ssd","user":{"_id":1},"createdAt":"2020-06-14T18:29:44.300Z","_id":"0484edae-2d13-427c-8606-db85ff90864c"}]
+    // [{"text":"Ssd","user":{"_id":1},"createdAt":"2020-06-14T18:29:44.300Z","_id":"0484edae-2d13-427c-8606-db85ff90864c"}]
     renderMessage(props) {
         const {
             currentMessage: { text: currText },
@@ -160,23 +159,16 @@ export default class ChatMain extends React.Component {
 
     render() {
         return (
-            <UserConsumer>
-                {value => {
-                    return (
-                        <GiftedChat
-                            messages={this.state.messages}
-                            onSend={messages => this.onSend(messages)}
-                            user={{
-                                _id: 1,
-                            }}
-                            placeholder='Mensagem'
-                            multiline={false}
-                            renderMessage={this.renderMessage}
-                        />
-                    )
-                }
-                }
-            </UserConsumer>
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={messages => this.onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+                placeholder='Mensagem'
+                multiline={false}
+                renderMessage={this.renderMessage}
+            />
         )
     }
 }
