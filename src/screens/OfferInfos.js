e5b9634a04a16c2ce35407d34102bc7bc09652c6
danialvesb <dan78615>
2020-06-15 21:56:54 -0300

    Ajustes no chat

--- src/screens/OfferInfos.js
+++ src/screens/OfferInfos.js
@@ -1,59 +1,87 @@
 import React from 'react'
 import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import RequestOfferConfirm from '../components/Modals/RequestOfferConfirm';
+import axios from 'axios'
+import {server, showMessage} from '../common';
 
-export  default function OfferInfos({ route, navigation }) {
-    const { data } = route.params
-    return (
-        <ScrollView style={ styles.container }>
-            <View style={styles.content}>
-                <View style={styles.data}>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>{ data.service_title }</Text>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>{ `${data.city} / ${data.uf} ` }</Text>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Qualificações</Text>
-                    <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>V/H: { data.amount }</Text>
+export  default class OfferInfos extends React.Component{
+    state = {
+        interactions: []
+    }
+    componentDidMount() {
+        const { data } = this.props.route.params
+        this.getInteractions(data.id)
+    }
 
-                    <View style={styles.description}>
-                        <Text style={{marginBottom: 5, marginLeft: 3}}>Descrição:</Text>
-                        <Text style={{fontSize: 15 , color: 'rgba(36,41,46,0.76)', flex: 5, margin: 4}}>{data.description}</Text>
+    async getInteractions(id) {
+        let header = {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'multipart/form-data;',
+            },
+        }
+        await axios.get(`${server}/services/offers/interactions/${id}`, header).then(response => {
+            this.setState({interactions: response.data})
+        }).catch(() => {
+            showMessage('Não foi possível carregar avaliações')
+        })
+    }
+
+
+    render() {
+        const { data } = this.props.route.params
+
+        return (
+            <ScrollView style={ styles.container }>
+                <View style={styles.content}>
+                    <View style={styles.data}>
+                        <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>{ data.service_title }</Text>
+                        <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>{ `${data.city} / ${data.uf} ` }</Text>
+                        <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>Qualificações</Text>
+                        <Text style={{fontSize: 20, color: 'rgba(36,41,46,0.76)'}}>V/H: { data.amount }</Text>
+
+                        <View style={styles.description}>
+                            <Text style={{marginBottom: 5, marginLeft: 3}}>Descrição:</Text>
+                            <Text style={{fontSize: 15 , color: 'rgba(36,41,46,0.76)', flex: 5, margin: 4}}>{data.description}</Text>
+                        </View>
+
                     </View>
-
-                </View>
-                <View style={styles.buttonsData}>
-                    <RequestOfferConfirm navigation={navigation} data={data}></RequestOfferConfirm>
-                </View>
-            </View>
-
-            <View style={styles.content}>
-                <View style={styles.headerComments}>
-                    <Text>Avaliações dos clientes do prestador {data.name}</Text>
-                    <View style={{marginRight: 40}}>
-                        <Text style={{fontSize: 40}}>4.7</Text>
-                    </View>
-                    <View>
-                        <Text>5 estrelas</Text>
-                        <Text>4 estrelas</Text>
-                        <Text>3 estrelas</Text>
-                        <Text>2 estrelas</Text>
-                        <Text>1 estrelas</Text>
-                    </View>
-                    <View style={{width: '100%', borderWidth: 1, marginTop: 10}}>
-
+                    <View style={styles.buttonsData}>
+                        <RequestOfferConfirm navigation={this.props.navigation} data={data}></RequestOfferConfirm>
                     </View>
                 </View>
-                <View>
-                    <Text>Filano </Text>
-                    <Text>Filano </Text>
-                    <Text>Filano </Text>
-                    <Text>Filano </Text>
-                    <Text>Filano </Text>
 
-                </View>
+                {this.state.interactions &&
+                    <View style={styles.content}>
+                        <View style={styles.headerComments}>
+                            <View style={{marginRight: 40}}>
+                                <Text style={{fontSize: 40}}>4.7</Text>
+                            </View>
+                            <View>
+                                <Text>5 estrelas</Text>
+                                <Text>4 estrelas</Text>
+                                <Text>3 estrelas</Text>
+                                <Text>2 estrelas</Text>
+                                <Text>1 estrelas</Text>
+                            </View>
+                            <View style={{width: '100%', borderWidth: 1, marginTop: 10}}>
 
+                            </View>
+                        </View>
+                        <View>
+                            <Text>Filano </Text>
+                            <Text>Filano </Text>
+                            <Text>Filano </Text>
+                            <Text>Filano </Text>
+                            <Text>Filano </Text>
 
-            </View>
-        </ScrollView>
-    )
+                        </View>
+                    </View>
+                }
+
+            </ScrollView>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
